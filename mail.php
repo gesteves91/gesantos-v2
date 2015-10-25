02c1e73957a569cc989d95598b9112a4a01076a6
gesteves91 <gesteves@avenuecode.com>
2015-10-25 18:13:43 -0200

    mail.php added

---
+++ mail.php
@@ -0,0 +1,30 @@
+<?php
+require 'PHPMailer/PHPMailerAutoload.php';
+ 
+$mail = new PHPMailer;
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.gmail.com';                       // Specify main and backup server
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'geanderson.esteves@gmail.com';     // SMTP username
+$mail->Password = 'rock=1257851';                        // SMTP password
+$mail->SMTPSecure = 'tls';                            // Enable encryption, 'ssl' also accepted
+$mail->Port = 587;                                    //Set the SMTP port number - 587 for authenticated TLS
+
+$mail->From = $_POST['email'];
+$mail->FromName = $_POST['name'];
+
+$mail->addAddress('esteves.gean@gmail.com', 'Geanderson Esteves');     // Add a recipient                                 
+ 
+$mail->Subject = $_POST['subject'];
+$mail->Body    = $_POST['name'] . "(" . $_POST['email'] . ")<br/>" . $_POST['message'];
+$mail->AltBody = $_POST['message'];
+$mail->isHTML(true);                                  // Set email format to HTML
+
+if(!$mail->send()) {
+   echo 'Message could not be sent.';
+   echo 'Mailer Error: ' . $mail->ErrorInfo;
+   exit;
+}
+
+?>
